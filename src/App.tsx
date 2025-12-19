e09c79baa0d33cf43abed25234348b791545fd99
truong tien dat <dd220302@gmail.com>
2025-12-20 02:07:25 +0700

    fix: update routing to correctly render NotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -14,9 +15,9 @@ const App = () => (
             <Sonner />
             <BrowserRouter>
                 <Routes>
-                    {/* <Route path="/" element={<Index />} /> */}
+                    <Route path="/" element={<Index />} />
                     {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                    <Route path="*" element={<Index />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
         </TooltipProvider>
